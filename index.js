2264077bab607c2fd3c683954aadeee38751cb4a
niloufar <zaferaniniloufar@gmail.com>
2022-01-01 09:13:41 +0330

    Upload simple vgA project on Github

---
+++ index.js
@@ -0,0 +1,8 @@
+//define number and convert
+var number1= Number(prompt("eneter num1",0))
+ var number2=Number(prompt("enter num2"),0)
+ var number3 =Number(prompt("enter num3",0))
+ //do avrage
+  var avg= number1+number2+number3/ 3;
+  //alert ang
+  alert(avg)
